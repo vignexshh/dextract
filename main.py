d9b2d6b39a5f9e02efe77fa4546e2940b46d4125
Vigensh T D <vigneshtd@outlook.com>
2026-03-02 20:41:44 +0530

    py-doctr

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("start_")
+import json
+
+from doctr.io import DocumentFile
+from doctr.models import ocr_predictor
+
+
+doc = DocumentFile.from_pdf("local_files/clg syllabus.pdf")
+print(f"Number of pages: {len(doc)}")
+
+predictor = ocr_predictor(pretrained=True)
+
+print(predictor)
+
+
+result = predictor(doc)
+
+synthetic_pages = result.synthesize()
+
+json_export = result.export()
+with open('local_files/output.json', 'w') as f:
+    json.dump(json_export, f)
